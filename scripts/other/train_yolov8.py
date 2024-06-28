99f198db34bacd8fb62c8d5c40b9cad7f958053a
Kobayashi0822 <bknb0822@gmail.com>
2024-06-28 14:52:34 +0900

    [UPD] directory

--- scripts/other/train_yolov8.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("yolov8n.pt")
-
-model.train(data="datasets/azure_rcjp2022/train.yaml", epochs=500, save=True,batch=4, imgsz=640)
-metrics = model.val()
-#sucsess = model.export(fromat="yolo")
